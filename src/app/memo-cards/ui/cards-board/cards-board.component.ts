6040c3fdfbeca063fb40fed643c1d94f6349d4c1
Mint Missy <dawidkostka480@gmail.com>
2023-01-13 21:18:28 +0100

    feat: cards board

--- src/app/memo-cards/ui/cards-board/cards-board.component.ts
+++ src/app/memo-cards/ui/cards-board/cards-board.component.ts
@@ -1,9 +1,14 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { MemoCard } from '../../model/memo-card.model';
 
 @Component({
-  selector: 'app-cards-board',
-  templateUrl: './cards-board.component.html',
-  styleUrls: ['./cards-board.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+	selector: 'app-cards-board',
+	templateUrl: './cards-board.component.html',
+	styleUrls: ['./cards-board.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CardsBoardComponent {}
+export class CardsBoardComponent {
+	@Input() memoCards!: MemoCard[];
+
+	@Output() flipCard = new EventEmitter<{ cardIndex: number }>();
+}
