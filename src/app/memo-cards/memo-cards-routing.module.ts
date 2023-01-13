bc465fa2a8a6fe12a94ab5734cb0ce9d27d9960f
Mint Missy <dawidkostka480@gmail.com>
2023-01-13 21:43:02 +0100

    feat: setup child routing for memo cards

--- src/app/memo-cards/memo-cards-routing.module.ts
+++ src/app/memo-cards/memo-cards-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MemoCardsContainerComponent } from './feature/memo-cards-container/memo-cards-container.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: MemoCardsContainerComponent }];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forChild(routes)],
+	exports: [RouterModule],
 })
-export class MemoCardsRoutingModule { }
+export class MemoCardsRoutingModule {}
